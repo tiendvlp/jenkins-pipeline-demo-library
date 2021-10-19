9e5205caeefa53a44be04e8b6c7b6b0dba4a4454
tiendvlp <tiendvlp@gmail.com>
2021-10-19 13:17:10 +0700

    edit

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -4,7 +4,7 @@ def call() {
     def tools = new GroovyScriptEngine( '.' ).with {
       loadScriptByName( '../src/PluginManager.groovy' )
     }
-    new tools().getName("Hello");
+    new PluginManager().getName("Hello");
     node {
       sh '''
         git version
