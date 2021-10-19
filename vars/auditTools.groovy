280ed07ff883015186989bfc1102a80666692069
tiendvlp <tiendvlp@gmail.com>
2021-10-19 15:58:57 +0700

    a

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -1,8 +1,4 @@
-import com.tiendvlp.groovylib.PluginManager;
-
 def call() {
-  def script = loadScript('com/tiendvlp/groovylib/PluginManager.groovy');
-  println(new PluginManager().getName("Hello"));
     node {
       sh '''
         git version
